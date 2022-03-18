96a72ce836176d67769237fde6d6c1130818d71f
haffi96 <haffimazhar96@gmail.com>
2022-03-18 14:50:41 +0000

    state

--- frontend/screens/TabOneScreen.tsx
+++ frontend/screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
@@ -7,21 +7,23 @@ import { RootTabScreenProps } from '../types';
 
 
 async function fetchData () {
-    const resp = await fetch("https://localhost:8000/profile");
+    const resp = await fetch("http://10.62.70.165:8000/profile");
     const data = await resp.json()
-    console.log(data)
-    return data
+    return JSON.stringify(data)
   };
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>): JSX.Element {
+  const [dataThing, setDataThing] = useState('')
+
   return (
     <View style={styles.container}>
       <Button
       title='ViewProfile'
       color="white"
-      onPress={fetchData}
+      onPress={async () => setDataThing(await fetchData())}
       />
       <Text style={styles.title}>Tab One</Text>
+      <Text>{dataThing}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
@@ -31,7 +33,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
